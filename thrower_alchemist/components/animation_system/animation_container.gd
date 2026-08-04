d93c9987427a55bc9cda111ce991ffe0b190f01d
DevGreem <ian.pichardo@hotmail.com>
2026-08-04 14:49:28 -0400

    feat: Finally works AnimationContainer

--- thrower_alchemist/components/animation_system/animation_container.gd
+++ thrower_alchemist/components/animation_system/animation_container.gd
@@ -3,8 +3,24 @@ extends Control
 
 class_name AnimationContainer
 
-func _notification(what: int) -> void:
+func _ready() -> void:
 	
-	if what == NOTIFICATION_CHILD_ORDER_CHANGED:
-		custom_minimum_size = get_combined_minimum_size()
-		GameDebugger.debug_log(AnimationContainer, "Changed minimun size")
+	if not child_order_changed.is_connected(_on_order_changed):
+		child_order_changed.connect(_on_order_changed)
+
+func _on_order_changed() -> void:
+	
+	var rect: Rect2 = Rect2()
+	
+	for child: Node in get_children():
+		if child is Control:
+			var r: Rect2 = Rect2(child.position as Vector2, child.size as Vector2)
+			
+			if rect.size == Vector2.ZERO:
+				rect = r
+				continue
+			
+			rect = rect.merge(r)
+
+	custom_minimum_size = rect.size
+	GameDebugger.debug_log(AnimationContainer, "Updated custom minimum size to = " + str(custom_minimum_size))
