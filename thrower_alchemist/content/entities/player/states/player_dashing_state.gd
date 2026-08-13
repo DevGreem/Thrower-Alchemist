282cb5e8e40d443345f0a4a87da46a70202eba37
DevGreem <ian.pichardo@hotmail.com>
2026-08-13 00:07:31 -0400

    feat: Finally works dash and added a tile that don't lest you walk on it

--- thrower_alchemist/content/entities/player/states/player_dashing_state.gd
+++ thrower_alchemist/content/entities/player/states/player_dashing_state.gd
@@ -15,29 +15,31 @@ class_name PlayerDashingState
 @export var cooldown: float
 var remaining_cooldown: float = 0.0
 
+var character: CharacterBody2D:
+	get: return agent
+
 var original_change_direction: bool
 var original_speed: float
+var original_layer: int
+
+var timer: Timer
 
 func _ready() -> void:
+	timer = Timer.new()
+	timer.one_shot = true
+	timer.wait_time = cooldown
+	add_child(timer)
 	
-	if move_transition is BaseHSMTransitionAdder:
-		if move_transition is HSMBidirectionalTransitionAdder:
-			move_transition.guard = can_dash
-			move_transition.register_transition()
-		else:
-			move_transition.guard = can_dash
-			move_transition.register_transition()
+	self.set_guard(can_dash)
 
 func can_dash() -> bool:
-	return remaining_cooldown <= 0.0
-
-func _process(delta: float) -> void:
-	remaining_cooldown -= delta
+	return timer.is_stopped()
 
 func _enter() -> void:
 	
 	original_change_direction = move_component.can_change_direction
 	original_speed = move_component.speed
+	original_layer = character.collision_mask
 
 	var expression: Expression = Expression.new()
 	expression.parse("original " + operator + " speed", ["original", "speed"])
@@ -50,20 +52,16 @@ func _enter() -> void:
 	
 	move_component.can_change_direction = directionable
 	move_component.speed = value
+	character.collision_mask ^= PhysicsLayers.mask(PhysicsLayers.Enum.GROUND)
+	GameDebugger.debug_log(PlayerDashingState, "New Character layers = " + str(character.collision_layer))
 	
-	get_tree().create_timer(dash_time).timeout.connect(_on_timeout_dash)
+	get_tree().create_timer(dash_time).timeout.connect(_choose_transition)
 
 func _exit() -> void:
-	remaining_cooldown = cooldown
-
-func _on_timeout_dash() -> void:
+	timer.start()
 	move_component.speed = original_speed
 	move_component.can_change_direction = original_change_direction
-	
-	if move_input.axis == Vector2.ZERO:
-		return
-	
-	move_transition.trigger()
+	character.collision_mask = original_layer
 
 func _choose_transition() -> void:
 	
